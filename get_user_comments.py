14abc93e4df6a8d2747716e7d0802966a911ea61
parsa <parsanintendo12@gmail.com>
2020-12-24 15:38:39 +0330

    - create get_user_id , get_last_post_media_id , check_user_have_comment definations - use media_n_coments instead of media_commetns

--- get_user_comments.py
+++ get_user_comments.py
@@ -3,17 +3,18 @@ from random import randint
 from time import sleep
 import errors
 #ali.prkhn
+#programmingofficial
 username = "funny.green.smile"
 password = "fun1382greensmile8990"
 
 bot = cl(username, password)
-for id in bot.get_post_media_id("ali.prkhn") :
+for id in bot.get_post_media_id("ali.prkhn"):
+    print(id , "------------------------------")
     try :
         for comment in bot.get_post_comments(id) :
             print(comment, "\n")
     except errors.NoComments :
         print("!!!!!!!!!!!!post have no comment!!!!!!!!!!!!")
     except errors.ClosedComments :
-        print("!!!!!!!!!!!!post have no comment!!!!!!!!!!!!")
-    print(id , "------------------------------")
-input()
+        print("!!!!!!!!!!!!comments for this post are closed!!!!!!!!!!!!")
+    break
--- user.py
+++ user.py
@@ -9,10 +9,12 @@ class client:
         self.user = Client(username, password)
         self.uuid = self.user.generate_uuid()
         self.selfid = self.user.authenticated_user_id
-    
+
+    def get_user_id (self, username) :
+        return self.user.username_info(username)["user"]["pk"]
+
     def followers(self, username):
-        user_id = self.user.username_info(username)["user"]["pk"]
-        
+        user_id = self.get_user_id(username)
         followers = self.user.user_followers(user_id ,self.uuid)
 
         #for index in range(0, len(followers["users"])) :
@@ -27,9 +29,9 @@ class client:
             yield list(map(lambda user: user["username"] , followers["users"]))
 
     def followings(self, username):
-       user_id = self.user.username_info(username)["user"]["pk"]
-        
+       user_id = self.get_user_id(username)
        followings = self.user.user_following(user_id ,self.uuid)
+
        yield list(map(lambda user: user["username"] , followings["users"]))
 
        while followings["next_max_id"] :
@@ -37,19 +39,22 @@ class client:
            followings = self.user.user_following(user_id ,self.uuid, max_id=followings["next_max_id"])
            yield list(map(lambda user: user["username"] , followings["users"]))
 
-    def get_user_comments(self):
-
-        pass
     def get_post_media_id(self, username):
-        user_id = self.user.username_info(username)["user"]["pk"]
+        user_id = self.get_user_id(username)
         post = self.user.user_feed(user_id)
         for id in list(map(lambda media_id : media_id['pk'] , post['items'])) :
             yield id
 
+    def get_last_post_media_id(self, username):
+        user_id = self.get_user_id(username)
+        post = self.user.user_feed(user_id)
+
+        return post["items"][0]["pk"]
+
     def get_post_comments (self, media_id) :
-        post = self.user.media_comments(media_id)
+        post = self.user.media_n_comments(media_id, n=200)
         try :
-            comments = list(map(lambda comments : comments["text"], post["comments"]))
+            comments = list(map(lambda comments : comments["text"], post))
             if comments :
                 for comment in comments :
                     yield comment
@@ -58,7 +63,17 @@ class client:
         except KeyError :
             raise errors.ClosedComments
 
+    def check_user_have_comment (self, uname_needle, uname_haystack) :
+        userid_needle = self.get_user_id(uname_needle)
+        media_id = self.get_last_post_media_id(uname_haystack)
+        post = self.user.media_n_comments(media_id, n=100)
+
+        users = list(map(lambda comments : comments["user_id"], post))
+        if userid_needle in users :
+            return True
+
+        return False
+
     def random_follow(self):
-        
         pass
 
