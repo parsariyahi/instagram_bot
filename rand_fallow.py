fad82ea34be361529ddc044590c131f2b9e9393f
parsariyahi <52409892+parsariyahi@users.noreply.github.com>
2022-03-09 12:49:59 +0330

    Update rand_fallow.py

--- rand_fallow.py
+++ rand_fallow.py
@@ -1,11 +1,9 @@
 from user import client as cl
-from random import randint
-from time import sleep
-from decorator import timer
-import defs as df
-username = "parsarh82"
-password = "parsa1981"
+
+username = "your username"
+password = "your pass"
 
 client = cl(username, password)
 
+# follow n users randomly
 print(client.random_follow_n_from_followers('ali.prkhn', n=10))
