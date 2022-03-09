c459636041b5373bba097a3939d93a1781e3f1df
parsariyahi <52409892+parsariyahi@users.noreply.github.com>
2022-03-09 12:42:30 +0330

    delete username and pass

--- get_user_comments.py
+++ get_user_comments.py
@@ -2,19 +2,21 @@ from user import client as cl
 from random import randint
 from time import sleep
 import errors
-#ali.prkhn
-#programmingofficial
-username = "funny.green.smile"
-password = "fun1382greensmile8990"
+
+
+username = "your username"
+password = "your pass"
 
 bot = cl(username, password)
-for id in bot.get_post_media_id("ali.prkhn"):
-    print(id , "------------------------------")
-    try :
-        for comment in bot.get_post_comments(id) :
-            print(comment, "\n")
-    except errors.NoComments :
-        print("!!!!!!!!!!!!post have no comment!!!!!!!!!!!!")
-    except errors.ClosedComments :
-        print("!!!!!!!!!!!!comments for this post are closed!!!!!!!!!!!!")
-    break
+
+def get_comments() :
+    for id in bot.get_post_media_id("ali.prkhn"):
+        print(id , "------------------------------")
+        try :
+            for comment in bot.get_post_comments(id) :
+                print(comment, "\n")
+        except errors.NoComments :
+            print("!!!!!!!!!!!!post has no comment!!!!!!!!!!!!")
+        except errors.ClosedComments :
+            print("!!!!!!!!!!!!comments for this post are closed!!!!!!!!!!!!")
+        break
